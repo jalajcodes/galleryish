e7ea2e940cd61366a219c1ecb2b0d7dcb631b398
Jalaj <Jalaj@mail.com>
2021-04-21 09:22:02 +0530

    fix logout upload error

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import { toast } from "react-toastify";
+import { useLocation } from "react-router-dom";
 import PersonalProgressBar from "./PersonalProgressBar";
 import ProgressBar from "./ProgressBar";
+import { useAuth } from "../hooks/useAuth";
 
-const UploadForm = ({ user }) => {
+const UploadForm = () => {
     const [file, setFile] = useState(null);
     const [error, setError] = useState(null);
+    const location = useLocation();
+    const { user } = useAuth();
 
     const types = ["image/png", "image/jpeg"];
 
@@ -20,13 +25,33 @@ const UploadForm = ({ user }) => {
         }
     };
 
+    const handleClick = (e) => {
+        if (!user) {
+            e.preventDefault();
+            toast("Please Login to start uploading photos.", { type: "error" });
+        }
+    };
+
     return (
         <form>
-            <h3>{user ? "Upload a Private Photo" : "Upload a Public Photo"}</h3>
-            <label>
-                <input type="file" onChange={handleChange} />
-                <span>+</span>
-            </label>
+            {location.pathname === "/myspace" ? (
+                <>
+                    <h3>Upload a Private Photo</h3>
+                    <label>
+                        <input type="file" onChange={handleChange} />
+                        <span>+</span>
+                    </label>
+                </>
+            ) : (
+                <>
+                    <h3>Upload a Public Photo</h3>
+                    <label>
+                        <input type="file" onChange={handleChange} onClick={handleClick} />
+                        <span>+</span>
+                    </label>
+                </>
+            )}
+
             <div className="output">
                 {error && <div className="error">{error}</div>}
                 {file && <div>{file.name}</div>}
